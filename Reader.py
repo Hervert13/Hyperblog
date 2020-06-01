466f5649e94b4adce590aa7229c4def378441ad5
joseluis.mejia <joseluis.mejia@sisamex.com.mx>
2020-06-01 14:11:35 -0500

    arreglando los exadecimales faltantes

--- Reader.py
+++ Reader.py
@@ -57,6 +57,10 @@ while continue_reading:
         uid3 = (hex(uid[3]).split('x')[-1], hex(uid[2]).split('x')[-1], hex(uid[1]).split('x')[-1], hex(uid[0]).split('x')[-1])
         print "UID invertido en HEX: ", uid3
 
+
+        for i in uid3:
+            print (i)
+
         uuidHEX = (str(uid3[0])+ str(uid3[1])+ str(uid3[2])+ str(uid3[3]))
         uuidDEC = int(uuidHEX, 16)
 
