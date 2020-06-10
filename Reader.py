fe17fccf60d02a93be044b4df3ebfc0112f5c9dd
joseluis.mejia <joseluis.mejia@sisamex.com.mx>
2020-06-10 08:44:24 -0500

    se agrega pausa para evitar registros repetidos

--- Reader.py
+++ Reader.py
@@ -50,51 +50,53 @@ print ("Welcome to the MFRC522 data read example")
 
 # This loop keeps checking for chips. If one is near it will get the UID and authenticate
 while continue_reading:
-
-    # Scan for cards
-    (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
-
-    # If a card is found
-    if status == MIFAREReader.MI_OK:
-        print ("Card detected")
-
-    # Get the UID of the card
-    (status,uid) = MIFAREReader.MFRC522_Anticoll()
-
-    # If we have the UID, continue
-    if status == MIFAREReader.MI_OK:
-
-        # print ("Card read UID: %s,%s,%s,%s" % (uid[0], uid[1], uid[2], uid[3]))
-        # print ("invirtiendo el sentido del UID")
-        uid3 = []
-        uid3 = (hex(uid[3]).split('x')[-1], hex(uid[2]).split('x')[-1], hex(uid[1]).split('x')[-1], hex(uid[0]).split('x')[-1])
-        # print ("UID invertido en HEX: ", uid3)
-
-        n = 0
-        for i in uid3:
-#            print (i)
-            if len(i) < 2:
-                uid3[n] = (str(0) + str(i))
-                # print (".......digito exadecimal corregido", uid3[n])
-            n= n + 1
-#        print ("los HEX completos son: ",  uid3)
-
-        uuidHEX = (str(uid3[0])+ str(uid3[1])+ str(uid3[2])+ str(uid3[3]))
-        uuidDEC = int(uuidHEX, 16)
-        # print ("UUID en la BD debe ser: ", uuidDEC)
-        
-        qryResult = qryConsultRFID(str(uuidDEC))
-        try:
-            cardNumber = str(qryResult[1][2])
-            employeeNumber = str(qryResult[1][1])
-            if employeeNumber == "None":
-                employeeNumber = " "
-            cardHolderName = str(qryResult[1][0])
-        
-        except:
-            print("no se encontró tarjeta en premisis")
+    try:
+        # Scan for cards
+        (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+    
+        # If a card is found
+        if status == MIFAREReader.MI_OK:
+            print ("Card detected")
+    
+        # Get the UID of the card
+        (status,uid) = MIFAREReader.MFRC522_Anticoll()
+    
+        # If we have the UID, continue
+        if status == MIFAREReader.MI_OK:
+    
+            # print ("Card read UID: %s,%s,%s,%s" % (uid[0], uid[1], uid[2], uid[3]))
+            # print ("invirtiendo el sentido del UID")
+            uid3 = []
+            uid3 = (hex(uid[3]).split('x')[-1], hex(uid[2]).split('x')[-1], hex(uid[1]).split('x')[-1], hex(uid[0]).split('x')[-1])
+            # print ("UID invertido en HEX: ", uid3)
+    
+            n = 0
+            for i in uid3:
+    #            print (i)
+                if len(i) < 2:
+                    uid3[n] = (str(0) + str(i))
+                    # print (".......digito exadecimal corregido", uid3[n])
+                n= n + 1
+    #        print ("los HEX completos son: ",  uid3)
+    
+            uuidHEX = (str(uid3[0])+ str(uid3[1])+ str(uid3[2])+ str(uid3[3]))
+            uuidDEC = int(uuidHEX, 16)
+            # print ("UUID en la BD debe ser: ", uuidDEC)
             
-        insert_transactions(cardNumber, employeeNumber, cardHolderName, doorId)
-        fecha   = str(datetime.datetime.now())
-        print(fecha, employeeNumber, cardHolderName)
-        sleep(1.5)
+            qryResult = qryConsultRFID(str(uuidDEC))
+            try:
+                cardNumber = str(qryResult[1][2])
+                employeeNumber = str(qryResult[1][1])
+                if employeeNumber == "None":
+                    employeeNumber = " "
+                cardHolderName = str(qryResult[1][0])
+            
+            except:
+                print("no se encontró tarjeta en premisis")
+                
+            insert_transactions(cardNumber, employeeNumber, cardHolderName, doorId)
+            fecha   = str(datetime.datetime.now())
+            print(fecha, employeeNumber, cardHolderName)
+            sleep(1.5)
+        except:
+            print("Error" )
